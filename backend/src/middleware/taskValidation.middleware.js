83a63344951fc7c91e5de3d4d2641e45bff22818
aditya6002 <adityaofficial690@gmail.com>
2026-02-20 18:57:46 +0530

    add task validation rules

---
+++ backend/src/middleware/taskValidation.middleware.js
@@ -0,0 +1,70 @@
+const { body, param, validationResult } = require("express-validator");
+const AppError = require("./AppError.middleware");
+
+const validateResult = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const msg = errors.errors[0].msg || "Server error";
+    throw new AppError(msg, 400);
+  }
+  next();
+};
+
+const createTaskValidationRules = [
+  body("title")
+    .isString()
+    .withMessage("Invalid  title")
+    .isLength({ min: 5, max: 40 })
+    .withMessage("Invalid title"),
+  body("content")
+    .isString()
+    .withMessage("Invalid content")
+    .isLength({ min: 1, max: 40 })
+    .withMessage("Invalid content"),
+  validateResult,
+];
+
+const editTaskValidationRules = [
+  body("task.userId")
+    .notEmpty()
+    .withMessage("UserId is required")
+    .isMongoId()
+    .withMessage("Invalid UserId"),
+
+  body("task.title")
+    .notEmpty()
+    .withMessage("Title is required")
+    .isLength({ min: 3, max: 40 })
+    .withMessage("Title must be at least 3 characters"),
+
+  body("task.content")
+    .notEmpty()
+    .withMessage("Content is required")
+    .isLength({ min: 1, max: 40 })
+    .withMessage("Content must be at least 5 characters"),
+
+  validateResult,
+];
+
+const getAllTaskValidationRules = [
+  param("skip").notEmpty().withMessage("Skip is required"),
+  param("limit").notEmpty().withMessage("Limit is required"),
+  validateResult,
+];
+
+const deleteTaskValidationRules = [
+  param("taskId")
+    .notEmpty()
+    .withMessage("Journal id is required")
+    .isMongoId()
+    .withMessage("Wrong journal id"),
+  validateResult,
+];
+
+module.exports = {
+  createRules: createTaskValidationRules,
+  editRules: editTaskValidationRules,
+  getRules: getAllTaskValidationRules,
+  deleteRules: deleteTaskValidationRules,
+};
--- backend/src/routes/tasks.routes.js
+++ backend/src/routes/tasks.routes.js
@@ -3,20 +3,35 @@ const { isUserLogin } = require("../middleware/auth.middleware.js");
 const tasksControllers = require("../controllers/tasks.controller.js");
 const wrapAsync = require("../middleware/wrapAsync.middleware.js");
 const router = express.Router();
+const taskValidationRules = require("../middleware/taskValidation.middleware.js");
 
 // Create and edit route
 router
   .route("/")
-  .post(isUserLogin, wrapAsync(tasksControllers.addNewTask))
-  .put(isUserLogin, wrapAsync(tasksControllers.editTask));
+  .post(
+    isUserLogin,
+    taskValidationRules.createRules,
+    wrapAsync(tasksControllers.addNewTask),
+  )
+  .put(
+    isUserLogin,
+    taskValidationRules.editRules,
+    wrapAsync(tasksControllers.editTask),
+  );
 
 // Delete route
-router.delete("/:taskId", isUserLogin, wrapAsync(tasksControllers.deleteTask));
+router.delete(
+  "/:taskId",
+  isUserLogin,
+  taskValidationRules.deleteRules,
+  wrapAsync(tasksControllers.deleteTask),
+);
 
 // Get route
 router.get(
   "/:skip/:limit",
   isUserLogin,
+  taskValidationRules.getRules,
   wrapAsync(tasksControllers.getAllTasks),
 );
 
