9201e23e449e72dbc90abb2def0ae318fb901290
aditya6002 <adityaofficial690@gmail.com>
2026-03-26 21:14:16 +0530

    Add auth store

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 // import AppProviders from "./app/app.providers";
 import { router } from "./app/app.routes";
 import GlobalStyles from "./shared/styles/GlobalStyles";
-// import { Provider } from "react-redux";
-// import { store } from "./store/store";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import { RouterProvider } from "react-router";
 // import { LoaderProvider } from "./shared/components/ui/GlobalLoader";
 // import { SubscriptionProvider } from "./features/subscription/subscription.context";
@@ -10,17 +10,16 @@ import { RouterProvider } from "react-router";
 
 export default function App() {
   return (
-    // <Provider store={store}>
-    // <LoaderProvider>
-    // <SubscriptionProvider>
-    // <ActivityProvider>
-    <>
+    <Provider store={store}>
+      // // {/* <LoaderProvider> */}
+      //// {/* <SubscriptionProvider> */}
+      // //
+      {/* <ActivityProvider> */}
       <GlobalStyles />
       <RouterProvider router={router} />
-    </>
-    // </ActivityProvider>
-    // </SubscriptionProvider>
-    // </LoaderProvider>
-    // </Provider>
+      //// {/* </ActivityProvider> */}
+      //// {/* </SubscriptionProvider> */}
+      //// {/* </LoaderProvider> */}
+    </Provider>
   );
 }
